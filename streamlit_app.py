cf06f048e030f77658b646ef5e8b709c429d7e84
sy-014 <a9chige7127@gmail.com>
2024-06-06 15:40:41 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,19 +27,25 @@ for msg in st.session_state.messages:
 # ユーザー入力の取得
 prompt = st.text_input("Your message:")
 
-if prompt:
-    # ユーザー入力をセッション状態に追加
-    st.session_state.messages.append({"role": "user", "content": prompt})
+user_msg = st.chat_input("ここにメッセージを入力")
+if user_msg:
+    # 以前のチャットログを表示
+    for chat in st.session_state.chat_log:
+        avator = avator_img_dict.get(chat["name"], None)
+        with st.chat_message(chat["name"], avatar=avator):
+            st.write(chat["msg"])
 
-## OpenAIモデルをロードし、応答を取得する関数
-def get_gemini_response(prompt):
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(prompt)
-    return response.text
+    # 最新のメッセージを表示
+    assistant_msg = "もう一度入力してください"
+    moriage_yaku_msg = "アンコール！アンコール！"
+    with st.chat_message(USER_NAME):
+        st.write(user_msg)
+    with st.chat_message(ASSISTANT_NAME):
+        st.write(assistant_msg)
+    with st.chat_message(MORIAGE_YAKU_NAME, avatar=avator_img_dict[MORIAGE_YAKU_NAME]):
+        st.write(moriage_yaku_msg)
 
-    # 応答をテキストとして取得（ここではresponse.textと仮定）
-    assistant_response = response.text
-
-    # 応答をセッション状態に追加し、表示
-    st.session_state.messages.append({"role": "assistant", "content": assistant_response})
-    st.text(f"Assistant: {assistant_response}")
+    # セッションにチャットログを追加
+    st.session_state.chat_log.append({"name": USER_NAME, "msg": user_msg})
+    st.session_state.chat_log.append({"name": ASSISTANT_NAME, "msg": user_msg})
+    st.session_state.chat_log.append({"name": MORIAGE_YAKU_NAME, "msg": user_msg})
