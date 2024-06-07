6d4c8ab478991763d748a51f288bfd836fdded60
sy-014 <a9chige7127@gmail.com>
2024-06-07 09:57:08 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,14 +17,11 @@ model = genai.GenerativeModel('gemini-pro')
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "Assistant", "content": "How can I help you?"}]
 
-# ユーザー入力の取得
-prompt = st.text_input("Your message:")
-
 # 既存のメッセージを表示
 for msg in st.session_state.messages:
     # ここではシンプルなテキスト表示を使用
     st.text(f"{msg['role']}: {msg['content']}")
-msg = {
-    'role': 'Assistant',
-    'content': 'Iam a large language model,trained by Google.'
-}
+
+# ユーザー入力の取得
+prompt = st.text_input("Your message:")
+
